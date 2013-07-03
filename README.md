c905e4dc890267e503bc38f7ac84522c1362e87c
Ian Allison <iana@pims.math.ca>
2013-07-03 10:47:58 -0700

    Updated solver to allow 4th and 6th order spatial derivatives
    Signed-off-by: Ian Allison <iana@pims.math.ca>

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 Solitons
 ========
+
+This directory contains codes to solve the Korteveg De Vries equation using
+finite difference methods. There are two independent codes: a C code and an
+ipython notebook. Both solve the problem in the same way (central differences
+for the spatial derivatives and a 4th order Runge Kutta for the time 
+derivative).
+
+Initial conditions are embedded in the code but are highlighted by comments and
+should be easy to change.
+
+C Code
+------
+The C code is all inside a single file, it uses malloc and math.h but otherwise
+should be pretty portable - just remember to add "-lm" to link in the math
+libraries. To compile it try...
+
+gcc -o kdv kdv.c -lm
+
+then to run it try
+
+./kdv > kdv.dat
+
+The resulting .dat file can be used to create an animated gif of your solution
+using the kdv.gnu gnuplot script. As long as gnuplot is installed, just running
+this script (./kdv.gnu) should output a file called kdv.gif with your solution
+animated - any web browser should be able to display it.
+
+
+iPython notebook
+----------------
+The ipython notebook should run on most versions of ipython but needs the numpy
+and matplotlib libraries installed. Like the C code, it will try to animate the
+solution it produces. The animation does not work with the plot embedded into
+the notebook at the moment but should open in a separate window. To run it
+change into the directory containing Solitons.ipynb and start ipython notebook
+
+ipython notebook
+
+Select the Solitons notebook and run all of the cells.
--- kdv.c
+++ kdv.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
@@ -21,18 +20,24 @@ int main(void) {
 
 	for(i = 0; i < N; i++) {
 	    x[i] = -L + i * DX;
+		xnew[i] = -L + i * DX;
 	}
 
-	/**
-	 * Initial Conditions, e.g. kdvExact
+	/* 
+	 * Initial Conditions
 	 */
-	kdvExact(x, 0., 16., 0);
+	kdvExact(x, 0., 16., 4.);
+	kdvExact(xnew, 0, 4., -4.);
+
+	for(i = 0; i < N; i++) {
+		x[i] = x[i] + xnew[i];
+	}
 
 	t = 0.;
 	j = 0;
 	while(t < 2) {
 
-		if((j % 500) == 0) {
+		if((j % 100) == 0) {
 	        for(i = 0; i < N; i++) {
 		        printf("%e %e\n", -L + i * DX, x[i]);
 	        }
@@ -60,8 +65,12 @@ void kdv(double x[], double xnew[], double dt, double dx) {
 	int i;
 	double *f1 = malloc(sizeof(double) * N);
 	double *f2 = malloc(sizeof(double) * N);
+	double *f3 = malloc(sizeof(double) * N);
+	double *f4 = malloc(sizeof(double) * N);
 	double *b1 = malloc(sizeof(double) * N);
 	double *b2 = malloc(sizeof(double) * N);
+	double *b3 = malloc(sizeof(double) * N);
+	double *b4 = malloc(sizeof(double) * N);
 	double *ux1 = malloc(sizeof(double) * N);
 	double *ux3 = malloc(sizeof(double) * N);
 
@@ -70,16 +79,31 @@ void kdv(double x[], double xnew[], double dt, double dx) {
 	}
 	f1[N-1] = x[0];
 
-	for(i = 1; i < N; i++) {
-		b1[i] = x[i-1];
-	}
-	b1[0] = x[N-1];
-
 	for(i = 0; i < N-2; i++) {
 		f2[i] = x[i+2];
 	}
 	f2[N-1] = x[1];
 	f2[N-2] = x[0];
+
+	for(i = 0; i < N-3; i++) {
+		f3[i] = x[i+3];
+	}
+	f3[N-1] = x[2];
+	f3[N-2] = x[1];
+	f3[N-3] = x[0];
+
+	for(i = 0; i < N-4; i++) {
+		f4[i] = x[i+4];
+	}
+	f4[N-1] = x[3];
+	f4[N-2] = x[2];
+	f4[N-3] = x[1];
+	f4[N-4] = x[0];
+
+	for(i = 1; i < N; i++) {
+		b1[i] = x[i-1];
+	}
+	b1[0] = x[N-1];
 
 	for(i = 2; i < N; i++) {
 		b2[i] = x[i - 2];
@@ -87,16 +111,55 @@ void kdv(double x[], double xnew[], double dt, double dx) {
 	b2[0] = x[N-2];
 	b2[1] = x[N-1];
 
+	for(i = 3; i < N; i++) {
+		b3[i] = x[i - 3];
+	}
+	b3[0] = x[N-3];
+	b3[1] = x[N-2];
+	b3[2] = x[N-1];
+
+	for(i = 4; i < N; i++) {
+		b4[i] = x[i - 4];
+	}
+	b4[0] = x[N-4];
+	b4[1] = x[N-3];
+	b4[2] = x[N-2];
+	b4[3] = x[N-1];
+
+
 	for(i = 0; i < N; i++) {
-		ux1[i] = (f1[i] - b1[i]) / (2. * dx);
+		/*
+		 * The O(h^2) central difference
+		 */
+		/*
+		ux1[i] =                         (f1[i] - b1[i]) / (2. * dx);
 		ux3[i] = (f2[i] - 2 * f1[i] + 2 * b1[i] - b2[i]) / (2. * dx * dx * dx);
+		*/
+
+		/*
+		 * The O(h^4) central difference
+		 */
+		/*
+		ux1[i] = (                      - (f2[i] - b2[i]) +  8. * (f1[i] - b1[i])) / (12. * dx);
+		ux3[i] = (- (f3[i] - b3[i]) + 8 * (f2[i] - b2[i]) - 13. * (f1[i] - b1[i])) / (8.  * dx * dx * dx);
+		*/
+		/*
+		 * The O(h^6) central difference
+		 */
+		ux1[i] = (                            (f3[i] - b3[i]) -   9. * (f2[i] - b2[i]) +  45. * (f1[i] - b1[i])) / ( 60. * dx);
+		ux3[i] = (7 * (f4[i] - b4[i]) - 72. * (f3[i] - b3[i]) + 338. * (f2[i] - b2[i]) - 488. * (f1[i] - b1[i])) / (240. * dx * dx * dx);
+
 		xnew[i] = -6. * x[i] * ux1[i] - ux3[i];
 	}
 
 	free(f1);
 	free(f2);
+	free(f3);
+	free(f4);
 	free(b1);
 	free(b2);
+	free(b3);
+	free(b4);
 	free(ux1);
 	free(ux3);
 }
